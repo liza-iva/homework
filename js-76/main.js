759d9532dccefd496a0cc8a168d4bd0b5b3c5939
liza-iva <ivanova-liza@ya.ru>
2024-06-16 21:19:31 +0300

    add js-76

---
+++ js-76/main.js
@@ -0,0 +1,14 @@
+// const firstName = 'John'
+// const lastName = 'Smith'
+// let age = '20'
+
+// console.log(firstName, lastName, age, 'y.o')
+
+const myName = 'Elizabeth'
+const mySurname = 'Ivanova'
+const age = '23'
+let jobSphere = 'Construction'
+let jobPosition = 'Design engineer'
+
+console.log(myName, mySurname, ',', age, 'y.o')
+console.log( 'Work area and position:', jobSphere, ',', jobPosition)
