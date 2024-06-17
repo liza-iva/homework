c66a6eb536e5b4d935c92e7d2c4ecd1f9ea9a11a
liza-iva <ivanova-liza@ya.ru>
2024-06-17 21:57:16 +0300

    add js-80

---
+++ js-80/main.js
@@ -0,0 +1,41 @@
+// let i = 0
+
+// while(i < 10) {
+//     i=i+1
+//     console.log(i)
+// }
+
+// ----------------------
+
+// do {
+//     i++
+//     console.log(i)
+// } while (i < 5)
+
+// ----------------------
+
+// for (let i = 1; i <= 10; i = i + 2) {
+//     console.log(`Пройден ${i} круг`)
+// }
+
+// -------------------------------DZ----------------------------------
+
+for (let i = 2; i <= 12; i = i + 2) {
+        console.log(i)
+    }
+
+// let i = 0
+// while(i < 12) {
+//     i=i+2
+//     console.log(i)
+// }
+
+for (let y = 1; y <= 10; y++) {
+    console.log(y <= 10 ? 11-y : y)
+}
+
+// let y = 0
+// while(y < 10) {
+//     y=y+1
+//     console.log(y <= 10 ? 11-y : y)
+// }
