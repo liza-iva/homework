52d1fc632f41b8f3dfb241ed4acc7b9c99f805df
liza-iva <ivanova-liza@ya.ru>
2024-06-17 21:56:50 +0300

    add js-79

---
+++ js-79/main.js
@@ -0,0 +1,43 @@
+// const firstNumber = 5
+// const secondNumber = 20
+
+// let result = null
+
+// if (firstNumber === 10 && secondNumber === 20) {
+//     result = firstNumber + secondNumber
+//     console.log(result)
+// } else {
+//     result = firstNumber - secondNumber
+//     console.log(result)
+// }
+
+// // ----------------------------------------------------
+
+// const alex = 'admin'
+// if (alex === 'user') {
+//     console.log('Alex is user')
+// } else if (alex === 'vip') {
+//     console.log('Alex is vip user')
+// } else {
+//     console.log('Alex is admin')
+// }
+
+// // ----------------------------------------------------
+
+// const jon = 'admin'
+
+// jon === 'admin' ? console.log('Jon is admin') : console.log('Jon in not admin')
+
+// // ----------------------------------------------------
+
+const age = 23
+
+if (age >= 18) {
+    console.log('Вы совершеннолетний')
+} else {
+    console.log('Вам еще нет 18')
+}
+
+// --------------------------------------
+
+age >= 18 ? console.log('Вы совершеннолетний') : console.log('Вам еще нет 18')
