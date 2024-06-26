c9cf68f3bf0a46ebee8c82037494bd2969d4ec7b
liza-iva <ivanova-liza@ya.ru>
2024-06-26 21:49:13 +0300

    add js-92

---
+++ js-92-dz/main.js
@@ -0,0 +1,25 @@
+const btn = document.querySelector('.btn-open')
+const modal = document.querySelector('.modal')
+
+const openModal = () => {
+    modal.classList.add('modal--open')
+}
+
+const closeModal = () => {
+    modal.classList.remove('modal--open')
+}
+
+btn.addEventListener('click', openModal)
+
+modal.addEventListener('click', event => {
+    const target = event.target
+    if(target && target.classList.contains('modal') || target.classList.contains('form__close-btn')) {
+        closeModal()
+    }
+})
+
+document.addEventListener('keydown', event => {
+    if (event.code === 'Escape' && modal.classList.contains('modal--open')) {
+        closeModal()
+    }
+})
